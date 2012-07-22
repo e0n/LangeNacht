60e8d7a3f7d6a80e7646bee16a408f45458d3ffc
Philip <phstaude@htwg-konstanz.de>
2012-07-22 14:02:06 +0200

    car group: current groupId added

--- GoGoGadgetoCar/src/gogo/gadgeto/car/CarGroupFragment.java
+++ GoGoGadgetoCar/src/gogo/gadgeto/car/CarGroupFragment.java
@@ -1,28 +1,39 @@
 package gogo.gadgeto.car;
 
+import gogo.gadgeto.model.Database;
 import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.view.View.OnClickListener;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class CarGroupFragment extends Fragment {
+	
+	private TextView currentGroupId;
 	
 	private Button joinGroupButton;
 	private Button leaveGroupButton;
 	private Button createGroupButton;
+	
+	private Database database;
 	
 	@Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
 		
 		View myView = inflater.inflate(R.layout.fragment_car_group, container, false);
 		
+		currentGroupId = (TextView) myView.findViewById(R.id.currentCarGroupIdEntryTextView);
 		joinGroupButton = (Button) myView.findViewById(R.id.joinCarGroupButton);
 		leaveGroupButton = (Button) myView.findViewById(R.id.leaveCarGroupButton);
 		createGroupButton = (Button) myView.findViewById(R.id.createCarGroupButton);
+		
+		database = Database.getInstance();
+		
+		currentGroupId.setText(database.getCurrentCarShareId());
 		
 		joinGroupButton.setOnClickListener(new OnClickListener() {			
 			public void onClick(View v) {
